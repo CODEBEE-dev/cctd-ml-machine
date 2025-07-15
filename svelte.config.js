3409dc0edf0f23481016f5708238ef03368e28dd
kyh174 <kyh174@naver.com>
2025-07-15 13:06:33 +0900

    base 추가
    svelte.config.js 설정 파일에 base 를 추가했습니다.

--- svelte.config.js
+++ svelte.config.js
@@ -8,5 +8,8 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 export default {
 	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
 	// for more information about preprocessors
-	preprocess: vitePreprocess()
+	preprocess: vitePreprocess(),
+  paths: {
+    base: process.argv.includes('build') ? '/ai/microbit' : '',
+  }
 };
